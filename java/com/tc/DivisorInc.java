422d84d01080e4c5e8c2d1c31b17bd5bf0dc0259
Aldwin Barredo <aldwinb@users.noreply.github.com>
2014-11-18 20:08:06 -0500

    adding stuff

--- java/com/tc/DivisorInc.java
+++ java/com/tc/DivisorInc.java
@@ -1,7 +1,39 @@
 package com.tc;
 
 public class DivisorInc {
-  public int countOperations(int N, int M) {
-    
+  private Set<Integer> marked;
+  private boolean pathFound;
+  private int level;
+
+  public DivisorInc() {
+    marked = new HashSet<Integer>();
+    level = 0;
   }
+
+  public int countOperations(int N, int M) {
+    dfs(N, M);
+    return level;
+  }
+
+  private void dfs(int v, int N) {
+    marked.add(v);
+    int d = 2
+      , half = v / 2;
+      , q = 0;
+
+    while (!pathFound) {
+      while (v % d != 0 && d <= half)
+        d++;
+      if (d > half) break;
+      q = v / d;
+      if (!marked.contains(v+q))
+        dfs(v+q);
+    }
+
+    if (!pathFound && n+q == N)
+      pathFound = true;
+
+    if (pathFound)
+      level++;
+  } 
 }
