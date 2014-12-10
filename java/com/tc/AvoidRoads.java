d854048b22418c88a7ad3206220c5c29a002d16e
aldwinb <aldwinb@daft.punk>
2014-12-09 22:29:42 -0500

    new stuff

---
+++ java/com/tc/AvoidRoads.java
@@ -0,0 +1,7 @@
+package com.tc;
+
+public class AvoidRoads {
+  public long numWays(int width, int height, String[] bad) {
+    
+  }
+}
--- java/com/tc/FlowerGarden.java
+++ java/com/tc/FlowerGarden.java
@@ -4,33 +4,34 @@ import java.util.*;
 
 public class FlowerGarden {
 
-  private class Flower {
+  private class Flower implements Comparable<Flower> {
     private int height;
     private int bloom;
     private int wilt;
 
-    public int outdeg;
+    public int indeg;
+    public List<Integer> adj;
 
     public Flower(int height, int bloom, int wilt) {
       this.height = height;
       this.bloom = bloom;
       this.wilt = wilt;
+      adj = new ArrayList<Integer>();
+      indeg = 0;
     }
 
-    public String toString() {
-      return String.format("%s,%s,%s", height, bloom, wilt);
+    public int compareTo(Flower f) {
+      if (indeg < f.indeg) return -1;
+      if (indeg == f.indeg) return height > f.height ? -1 : 1;
+      return 1;
     }
   }
 
   private Flower[] flowers;
-  private int[][] marked;
-  private int[] outdeg;
 
   public int[] getOrdering(int[] height, int[] bloom, int[] wilt) {
     int N = height.length;
     flowers = new Flower[N];
-    marked = new int[N][N];
-    outdeg = new int[N];
 
     for (int i = 0; i < N; i++) {
       flowers[i] = new Flower(height[i], bloom[i], wilt[i]);
@@ -38,28 +39,35 @@ public class FlowerGarden {
 
     for (int i = 0; i < N; i++) {
       for (int j = i; j < N; j++) {
-        if (!free(flowers[i], flowers[j]))
-          if (
+        if (!free(flowers[i], flowers[j])) {
+          boolean iLess = flowers[i].height < flowers[j].height;
+          flowers[iLess ? i : j].adj.add(iLess ? j : i);
+          flowers[iLess ? j : i].indeg++;
+        }
       }
     }
 
-    for (int i = 0; i < N; i++) {
-      sink(i);
-    }
-
+    PriorityQueue<Flower> pq = new PriorityQueue<Flower>();
+    for (int i = 0; i < N; i++)
+      pq.add(flowers[i]);
+    
     int[] order = new int[N];
     int i = 0;
-    Queue<Block> parentsPq = new PriorityQueue<Block>(parents);
-    while (!parentsPq.isEmpty()) {
-      Block b  = parentsPq.poll();
-      for (int j = b.first; j != -1; j = next[j]) {
-        //System.out.println(String.format("next = %s", j));
-        order[i++] = flowers[j].height;
+    while (!pq.isEmpty()) {
+      Flower f = pq.poll();
+      order[i++] = f.height;
+      for (Integer v : f.adj) {
+        if (pq.remove(flowers[v])) {
+          flowers[v].indeg--;
+          pq.add(flowers[v]);
+        }
       }
     }
+
     return order;
   }
   
   private boolean free(Flower m, Flower n) {
     return m.wilt < n.bloom || m.bloom > n.wilt;
   }
+}
