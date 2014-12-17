33e0ee60325eaa04b52990804fb032b86a29fa1b
aldwinb <aldwinb@daft.punk>
2014-12-16 21:54:36 -0500

    changes

--- java/com/tc/Jewelry.java
+++ java/com/tc/Jewelry.java
@@ -3,35 +3,42 @@ package com.tc;
 import java.util.*;
 
 public class Jewelry {
-  private Hashtable<Integer, Integer> sums;
+  private Hashtable<Integer, NavigableSet<Integer>> sums;
   private int[] max;
   private int[] values;
 
   public long howMany(int[] values) {
     this.values = values;
-    sums = new Hashtable<Integer, Integer>();
+    sums = new Hashtable<Integer, NavigableSet<Integer>>();
     max = new int[values.length];
     Arrays.sort(this.values);
     Arrays.fill(max, -1);
+    sums.put(0, null);
     return count(this.values.length-1);
   }
 
   private int count(int v) {
     if (v == 0) {
-      sums.put(values[v], 1);
+      NavigableSet<Integer> n = new TreeSet<Integer>();
+      n.add(1);
+      sums.put(values[v], n);
       max[v] = values[v];
       return 0;
     }
 
     int c = count(v-1);
     int right = values[v];
-    for (int i = max.length-1; i >= 0; i--) {
+    for (int i = values.length-1; i >= 0; i--) {
       if (max[i] < right) break;
       if (sums.containsKey(right)) {
-        c += sums.get(right);
-        break;
+        NavigableSet<Integer> lows = sums.get(right);
+        c += lows.size();
+        for (Integer l : lows.descendingSet()) {
+          if (l < right) break;
+          if (l == right) c++;
+        }
       }
-      right += max[i];
+      right += values[i];
     }
 
     int maxSum = 0;
@@ -41,7 +48,8 @@ public class Jewelry {
       if (sum > maxSum) maxSum = sum;
       if (!sums.containsKey(sum)) sums.put(sum, 1);
       else {
-        int val = sums.remove(sum);
+        sums.get(sum)
+        sums.remove(sum);
         sums.put(sum, val+1);
       }
     }
