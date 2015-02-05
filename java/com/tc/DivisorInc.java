88e5921ba652dd9e3dc4a91dfd68c543492c30f4
aldwinb <aldwinb@daft.punk>
2015-02-04 22:31:21 -0500

    changes

--- java/com/tc/DivisorInc.java
+++ java/com/tc/DivisorInc.java
@@ -3,17 +3,48 @@ package com.tc;
 import java.util.*;
 
 public class DivisorInc {
-  private Set<Integer> marked;
+    public int countOperations(int N, int M) {
+        if (N == M) return 0;
+        int lvl = 0;
+        boolean found = false;
+        
+        boolean[] marked = new boolean[100000];
+        for (int i = 0; i < 100000; i++)
+            marked[i] = false;
 
-  public DivisorInc() {
-    marked = new HashSet<Integer>();
-  }
+        Queue<Integer> q = new ArrayDeque<Integer>();
+        q.add(N);
+        while (!q.isEmpty() && !found) {
+            int v = q.poll();
+            List<Integer> adj = new ArrayList<Integer>();
+            for (int i = 2; i <= v/2; i++) {
+                if (v % i == 0) 
+                    adj.add(v+(v/i));
+            }
+            for (int w : adj) {
+                //System.out.println(String.format("v = %s, adj = %s", v, w));
+                if (w == M) {
+                    found = true;
+                    break;
+                }
+                if (!marked[w]) {
+                    q.add(w);
+                    marked[w] = true;
+                }
+            }
+            if (adj.size() > 0) lvl++;
+        }
+        return lvl;
+    }
 
-  public int countOperations(int N, int M) {
-    if (N == M) return 0;
-    return dfs(N, M, 1);
-  }
+    public static void main(String[] args) {
+        System.out.println(String.format("count = %s",
+            new DivisorInc().countOperations(
+                Integer.parseInt(args[0]),
+                Integer.parseInt(args[1]))));
+    }
 
+    /*
   private int dfs(int v, int N, int level) {
     marked.add(v);
     int returnLevel = -1;
@@ -43,15 +74,15 @@ public class DivisorInc {
     return returnLevel;  
   }
 
-  private int ld(int v) {
-    if (v % 2 == 0) return 2;
-    if (v % 3 == 0) return 3;
-    for (int i = 5; i * i <= v; i += 6) {
-      if (v % i == 0) return i;
-      if (v % (i + 2) == 0) return i + 2;
+    private int ld(int v) {
+        if (v % 2 == 0) return 2;
+        if (v % 3 == 0) return 3;
+        for (int i = 5; i * i <= v; i += 6) {
+            if (v % i == 0) return i;
+            if (v % (i + 2) == 0) return i + 2;
+        }
+        return -1;
     }
-    return -1;
-  }
 
   private int nextd(int v, int d, int hid) {
     while (v % d != 0 && d <= hid) {
@@ -66,4 +97,5 @@ public class DivisorInc {
     if (N < mid) return search(lo, mid-1, v, N);
     return v % (N-v) == 0;
   }
+    */
 }
