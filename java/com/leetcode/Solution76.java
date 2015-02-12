276de11fcb274c1d4932a2be18df49b4de7d1be7
aldwinb <aldwinb@daft.punk>
2015-02-11 22:32:55 -0500

    updates

--- java/com/leetcode/Solution76.java
+++ java/com/leetcode/Solution76.java
@@ -2,13 +2,23 @@ package com.leetcode;
 
 public class Solution76 {
     public String minWindow(String S, String T) {
-    
+        int R = S.length(),
+            M = S.length(),
+            Q = 997;
+        System.out.println(String.format("hash = %s", hash(S, R, M, Q)));
+        return "";
     }
 
-    private int hash(String p, int Q) {
-        int h = p.charAt(0) % Q;
-        for (int i = 1; i < p.length(); i++)
-            h = (h * 10) + (p.charAt(i) % Q);
+    private int hash(String p, int R, int M, int Q) {
+        int h = 0;//p.charAt(0) % Q;
+        for (int i = 0; i < M; i++) {
+            h = (h * R + p.charAt(i)) % Q;
+            System.out.println(String.format("charAt = %s, h = %s", p.charAt(i), h));
+        }
         return h;
+    }
+
+    public static void main(String[] args) {
+        new Solution76().minWindow(args[0], args[1]);    
     }
 }
