66632b3241854e35d3b2255807d7c3b6d5d27ec7
aldwinb <aldwinb@daft.punk>
2015-03-05 23:19:33 -0500

    changes

--- java/com/leetcode/Solution30.java
+++ java/com/leetcode/Solution30.java
@@ -10,38 +10,40 @@ public class Solution30 {
         Map<String, Integer> c = getCounts(L);
         int n = L[0].length();
         List<Integer> idxs = new ArrayList<Integer>();
-        Queue<String> q = new LinkedList<String>();
         for (int i = 0; i+L.length <= S.length(); i++) {
-            Queue<String> q = queueMatches(S,i,c);
+            Queue<String> q = queueMatches(S,i,n,c);
+            //System.out.format("q.size = %s\n", q.size());
             if (q.size() == L.length) idxs.add(i);
             putBack(q,c);
-            /*
-            while (!q.isEmpty()) {
-                String s2 = q.poll();
-                c.put(s2, c.remove(s2)+1);
-            }
-            */
         }
-        
         return idxs;
     }
 
-    private Queue<String> queueMatches(String S, int i, Map<String,Integer> c) {
-            /*
-            String s1 = S.substring(i,i+n);
-            //System.out.format("s1 = %s\n", s1);
-             
-            if (c.containsKey(s1) 
-            int j = i+n;
-            while (c.containsKey(s1) && c.get(s1) > 0) {
-                q.add(s1);
-                c.put(s1, c.remove(s1)-1);
-                if (j+n < S.length()) {
-                    s1 = S.substring(j,j+n);
-                    j += n;
-                } else s1 = null;
-            }ri
-            */
+    private void putBack(Queue<String> q, Map<String,Integer> c) {
+        while (!q.isEmpty()) {
+            String s2 = q.poll();
+            c.put(s2, c.remove(s2)+1);
+        }
+    }
+
+    private Queue<String> queueMatches(String S, 
+        int i, 
+        int n,
+        Map<String,Integer> c) {
+        Queue<String> q = new LinkedList<String>();
+        int j = i,
+            m = S.length();
+        if (j+n > m) return q;
+        String s1 = S.substring(j,j+n);
+        while (c.containsKey(s1) && c.get(s1) > 0) {
+            q.add(s1);
+            c.put(s1, c.remove(s1)-1);
+            j += n;
+            //System.out.format("j = %s\n", j);
+            if (j+n > m) break;
+            s1 = S.substring(j,j+n);
+        }
+        return q;
     }
 
     private Map<String, Integer> getCounts(String[] L) {
