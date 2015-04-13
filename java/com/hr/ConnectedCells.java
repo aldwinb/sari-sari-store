5913a182806a0f01605dc42d66d9d7af47bcff7e
aldwinb <aldwinb@daft.punk>
2015-04-13 07:09:37 -0400

    changes

--- java/com/hr/ConnectedCells.java
+++ java/com/hr/ConnectedCells.java
@@ -35,18 +35,6 @@ public class ConnectedCells {
         }
     }
     
-    // 0 1 1 1 0
-    // 1 1 0 1 0
-    //
-    // y: 0
-    // x: 0-1
-    //
-    // y: 0
-    // x: 3-3
-    //
-    // y: 1
-    // x: 1-3
-    //
     public int maxRegion(int[][] G) {
         if (G.length == 0 || G[0].length == 0) return 0;
         if (G.length == 1 && G[0].length == 1) return G[0][0];
@@ -58,15 +46,25 @@ public class ConnectedCells {
             int s = -1;
             List<Range> newEnds = new ArrayList<Range>();
             for (int j = 0; j < r.length; j++) {
-                if (r[j] == 1 && s == -1) s = j;
-                else if (s != -1) {
-                    newEnds.add(new Range(s, j-1));
-                    s = -1;
+                if (r[j] == 1) { 
+                    if (s == -1) s = j;
+                } else { 
+                    //System.out.format("r[j] = %s\n", r[j]);
+                    if (s != -1) {
+                        newEnds.add(new Range(s, j-1));
+                        s = -1;
+                    }
                 }
             }
+
             if (s != -1)
                 newEnds.add(new Range(s, r.length-1));
             max = Math.max(max, expandRegions(regions, newEnds, i));
+            /*
+            for (Region r1 : regions)
+                System.out.format("r.count = %s\n", r1.count);
+            System.out.println("");
+            */
         }
         return max;
     }
@@ -75,8 +73,8 @@ public class ConnectedCells {
         List<Region> regions, 
         Iterable<Range> newEnds,
         int lvl) {
+
         Set<Integer> marked = new HashSet<Integer>();
-        
         int max = 0;
         for (Range e : newEnds)
             for (Region r : regions)
@@ -90,6 +88,7 @@ public class ConnectedCells {
             if (!marked.contains(e.x2)) {
                 Region r = new Region(e, lvl);
                 regions.add(r);
+                //System.out.format("x2 = %s, x1 = %s, count = %s\n", e.x2, e.x1, r.count);
                 max = Math.max(max, r.count);
             }
 
@@ -107,10 +106,9 @@ public class ConnectedCells {
         ConnectedCells cc = new ConnectedCells();
         int ctr = 1;
         Scanner s = new Scanner(System.in);
-        for (int c = 1; s.hasNextLine(); c++) {
-            String line = s.nextLine();
-            String[] testCase = line.split("\\t");
-            String[] rows = testCase[0].split("|");
+        while (s.hasNextLine()) {
+            String[] testCase = s.nextLine().split("\\t");
+            String[] rows = testCase[0].split("\\|");
             int[][] G = new int[rows.length][rows[0].split(",").length];
             for (int i = 0; i < rows.length; i++) {
                 String[] cols = rows[i].split(",");
@@ -119,11 +117,20 @@ public class ConnectedCells {
             }
             int actual = cc.maxRegion(G),
                 expected = Integer.parseInt(testCase[1]);
-            if (expected != actual)
-                System.out.format("Line %s:\texpected: %s\tactual: %s\n",
-                    c,
+            if (expected != actual) {
+                printGrid(G);
+                System.out.format("expected: %s\tactual: %s\n",
                     expected,
                     actual);
+            }
+        }
+    }
+
+    private static void printGrid(int[][] G) {
+        for (int i = 0; i < G.length; i++) {
+            for (int j = 0; j < G[i].length; j++)
+                System.out.format("%s ", G[i][j]);
+            System.out.println("");
         }
     }
 }
--- java/com/leetcode/Solution115.java
+++ java/com/leetcode/Solution115.java
@@ -4,31 +4,6 @@ import java.util.*;
 
 public class Solution115 {
     public int numDistinct(String S, String T) {
-
-        /*
-        arrd
-         fayr2njar1d
-      a  0111 1122 2
-      r2 0001 1115 5
-      d     000003
-
-         fayrrnjard_
-       a 0111111222
-       r 0001222244
-       r 0000111133
-       d 0000000003
-
-        _arrr__ard
-        0111111222
-          01333366
-             00006
-        
-        ard
-        farnjard
-
-        arrrjard
-        ard
-        */
         if (T.length() > S.length()) return 0;
         int[] dp1 = new int[S.length()+1],
             dp2 = new int[S.length()+1];
@@ -36,11 +11,12 @@ public class Solution115 {
         Arrays.fill(dp2, 0);
         for (int i = 0; i < T.length(); i++) {
             for (int j = 1; j <= S.length(); j++)
-                if (T.charAt(i) == S.charAt(j))
+                if (T.charAt(i) == S.charAt(j-1))
                     dp2[j] = dp1[j-1]+dp2[j-1];
                 else
                     dp2[j] = dp2[j-1];
             dp1 = dp2;
+            dp2 = new int[S.length()+1];
         }
 
         return dp1[S.length()];
@@ -55,7 +31,8 @@ public class Solution115 {
             int expected = Integer.parseInt(testCase[1]),
                 actual = soln.numDistinct(input[0], input[1]);
             if (expected != actual)
-                System.out.format("Test case = %s, expected = %s, actual = %s",
+                System.out.format(
+                    "Test case = %s, expected = %s, actual = %s\n",
                     testCase[0],
                     expected,
                     actual);
