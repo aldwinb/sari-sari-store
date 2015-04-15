b074207cf4e7b00f282cc2158c9079df8603c19e
aldwinb <aldwinb@daft.punk>
2015-04-15 06:26:22 -0400

    changes

--- python/tc/StripePainter.py
+++ python/tc/StripePainter.py
@@ -11,28 +11,36 @@ class StripePainter:
         for i in range(len(stripes)):
             self.__cost.append(sys.maxint)
         return self.__cut(stripes, 0)
-        # return self.__getMin(stripes)
+        # return self.__getMinStrokes(stripes)
 
-    def __cut(self, stripes, i):
-        if len(stripes) <= 1:
-            return len(stripes)
-        if self.__cost[i] != sys.maxint:
-            return self.__cost[i]
+    # BECBBDDEEBABDCADEAAEABCACBDBEECDEDEACACCBEDABEDADD
+    # B__BB____B_B_________B___B_B____________B___B_____ = 1
+    # B__BBDDEEB_B__A__AA_ABC_CB_BEE__E_E_____B___B_D_DD = 7
+    # B__BBDDEEB_B__A__AA_ABC_CB_BEE__E_E_C_CCB___B_D_DD = 8
+    def __cut(self, stripes, j):
+        if len(stripes)-j <= 1:
+            return len(stripes)-j
+        if self.__cost[j] != sys.maxint:
+            return self.__cost[j]
         min = sys.maxint
-        for i in range(1, len(stripes)):
-            stroke = stripes[i:]
+        for i in range(j+1, len(stripes)):
+            # stroke = stripes[i:]
             # print 'cut = %s %s' % (stripes[:i], stroke)
-            left = self.__getMin(stripes[:i])
-            right = self.__cut(stroke, i)
-            print 'cut_l = %s, cnt_l = %s, cut_r = %s, cnt_r = %s' \
-                % (stripes[:i], left,  stroke, right)
+            left = self.__getMinStrokes(stripes[j:i])
+            right = self.__cut(stripes, i)
+            # print 'i = %s, cut_l = %s, cnt_l = %s, cut_r = %s, cnt_r = %s' \
+            #     % (i, stripes[j:i], left,  stroke, right)
             c = left+right
             if (min > c):
                 min = c
-        self.__cost[i] = c
-        return self.__cost[i]
+        c = self.__getMinStrokes(stripes[j:])
+        if (min > c):
+            min = c
+        # print 'cost = %s' % min
+        self.__cost[j] = min
+        return self.__cost[j]
 
-    def __getMin(self, stripes):
+    def __getMinStrokes(self, stripes):
         # print "stripes = %s" % stripes
         stroke = stripes[0]
         new_stroke = -1
@@ -41,12 +49,12 @@ class StripePainter:
             if stroke != stripes[i] and new_stroke == -1:
                 new_stroke = i
             elif stroke == stripes[i] and new_stroke != -1:
-                c = c+self.__getMin(stripes[new_stroke:i])
+                c = c+self.__getMinStrokes(stripes[new_stroke:i])
                 # print "count = %s" % c
                 new_stroke = -1
 
         if new_stroke != -1:
-            c = c+self.__getMin(stripes[new_stroke:i+1])
+            c = c+self.__getMinStrokes(stripes[new_stroke:i+1])
         # print "count = %s" % c
         return c
 
