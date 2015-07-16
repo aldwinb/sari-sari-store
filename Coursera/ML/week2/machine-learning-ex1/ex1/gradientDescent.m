da20bb96bedcc8023b73cd3482ecdadcdf5b0255
aldwinb <aldwinb@daft.punk>
2015-07-15 22:29:38 -0400

    changes

--- Coursera/ML/week2/machine-learning-ex1/ex1/gradientDescent.m
+++ Coursera/ML/week2/machine-learning-ex1/ex1/gradientDescent.m
@@ -16,18 +16,17 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
+    
+    newt = theta; 
+    theta(1) = newt(1) - (sum(((X*(newt)).-y).*X(:,1)))*alpha/m;
+    theta(2) = newt(2) - (sum(((X*(newt)).-y).*X(:,2)))*alpha/m;
+    % disp(theta);
 
     % ============================================================
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
-
+    % disp(sprintf('J: %f', J_history(iter)));
 end
 
 end
