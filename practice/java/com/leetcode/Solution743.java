6b0fe7ed54d5dce7439ded5fcbe23c0422a585c3
Aldwin Barredo <aldwinb@users.noreply.github.com>
2018-02-21 17:10:41 -0500

    Update network time delay solution

--- practice/java/com/leetcode/Solution743.java
+++ practice/java/com/leetcode/Solution743.java
@@ -3,60 +3,68 @@ package com.leetcode;
 import java.util.*;
 
 public class Solution743 {
-    private class AdjacentNode {
-        public final int node,
+    private class Node {
+        public final int id,
             cost;
-        
-        public AdjacentNode(int node, int cost) {
-            this.node = node;
+
+        public Node(int id, int cost) {
+            this.id = id;
             this.cost = cost;
         }
     }
-    
+
     private class Graph {
-        private Map<Integer, Queue<AdjacentNode>> _nodes;
-        
+        private Map<Integer, Queue<Node>> _nodes;
+
         public Graph(int[][] nodes) {
-            _nodes = new HashMap<Integer, Queue<AdjacentNode>>();
-            for (int[] node : nodes) 
-                _nodes.computeIfAbsent(node[0], k -> new LinkedList<AdjacentNode>()).add(new AdjacentNode(node[1], node[2]));
+            _nodes = new HashMap<Integer, Queue<Node>>();
+            for (int[] node : nodes)
+                _nodes.computeIfAbsent(node[0], k -> new ArrayDeque<Node>()).add(new Node(node[1], node[2]));
         }
-        
-        public Queue<AdjacentNode> getAdj(int v) {
-            return _nodes.get(v);
+
+        public Queue<Node> getAdj(int v) {
+            return _nodes.getOrDefault(v, new ArrayDeque<Node>());
         }
     }
-   
-    // private int _visited;
-    private Set<Integer> _visited; 
-    private Queue<AdjacentNode> _nodes;
 
-    private int dfs(Graph g, int v) {
-        _visited.add(v);
+    private Queue<Integer> _nodes;
+    private Set<Integer> _visited;
+    private int[] _costs;
 
-        Queue<AdjacentNode> adj = g.getAdj(v);
-        int cost = 0;
-        while (adj != null && !adj.isEmpty()) {
-            AdjacentNode n = adj.poll();
-            if (!_visited.contains(n.node))
-              cost = Math.max(dfs(g, n.node) + n.cost, cost);
+    private int bfs(Graph g, int s) {
+        int maxCost = 0;
+        _nodes.add(s);
+        _visited.add(s);
+        _costs[s] = 0;
+        while (!_nodes.isEmpty()) {
+            int nodeId = _nodes.poll();
+            for (Node n : g.getAdj(nodeId)) {
+                if (n.id != s) {
+                    if (_costs[n.id] > _costs[nodeId]+n.cost) {
+                        _costs[n.id] = _costs[nodeId]+n.cost;
+                        maxCost = Math.max(_costs[n.id], maxCost);
+                        System.out.println(String.format("node = %s, cost = %s, maxCost = %s", n.id, _costs[n.id], maxCost));
+                    }
+                    if (!_nodes.contains(n.id))
+                        _nodes.add(n.id);
+                }
+            }
         }
-        //_visited++;
-        return cost;
+        return maxCost;
     }
 
     public int networkDelayTime(int[][] times, int N, int K) {
         if (times.length == 0)
             return 0;
-        
-        // if (times.length == 1)
-        //     return times[0][2];
-        
-        Graph g = new Graph(times);
-        _visited = new HashSet<Integer>();
 
-        int cost = dfs(g, K);
-        
+        Graph g = new Graph(times);
+        _nodes = new ArrayDeque<Integer>();
+        _visited = new HashSet<Integer>();
+        _costs = new int[N+1];
+        Arrays.fill(_costs, 101);
+
+        int cost = bfs(g, K);
+
         return _visited.size() == N ? cost : -1;
     }
 
